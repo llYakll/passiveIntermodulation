787da8e488db224ee5bbf19cb7a40b8820c28044
llYakll <zacharycsullivan@gmail.com>
2024-05-24 12:19:37 -0500

    added route to create a new cat

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -31,17 +31,29 @@ router.get('/:id', async (req, res) => {
 
     res.status(200).json(category);
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    res.status(500).json({ message: err.message }); 
   }
 });
 
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const category = await Category.create(req.body);
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 });
+  
+  
+
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  try {
+    const category = await Category.create(req.body);
+  }
 });
 
 router.delete('/:id', (req, res) => {
